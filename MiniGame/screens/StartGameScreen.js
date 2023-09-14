09c71eb031877e5b784462291bbb05c22f9e2ca4
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-14 17:59:53 -0400

    just some buttons move along

---
+++ MiniGame/screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { View, TextInput } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
