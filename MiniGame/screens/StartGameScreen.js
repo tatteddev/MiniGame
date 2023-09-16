61bb87ea9cc8e60ef9d6272a356f38b5339a601a
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-16 09:10:33 -0400

    little style

--- MiniGame/screens/StartGameScreen.js
+++ MiniGame/screens/StartGameScreen.js
@@ -1,14 +1,22 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    inputContainer: { 
+      flex: 1,
+      marginTop: 100,
+      padding: 16,
+    }
+});
 
 export default StartGameScreen;
