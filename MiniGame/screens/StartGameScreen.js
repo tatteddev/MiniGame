cc4771d8af67a79edd555f445ad4c421cf7588cc
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-16 09:26:12 -0400

    styling

--- MiniGame/screens/StartGameScreen.js
+++ MiniGame/screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.textInput} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,6 +26,17 @@ const styles = StyleSheet.create({
       },
       shadowRadius: 6,
       shadowOpacity: 0.26,
+    },
+    textInput: {
+      height: 50,
+      width: 50,
+      fontSize: 32,
+      borderBottomColor: "#ddb52f",
+      borderBottomWidth: 2,
+      color: "#ddb52f",
+      marginVertical: 8,
+      fontWeight: "bold",
+      textAlign: "center"
     }
 });
 
