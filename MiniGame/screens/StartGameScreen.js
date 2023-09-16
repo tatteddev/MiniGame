8a3f18cfd7b7503392fe1d196ca671942061abce
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-16 09:18:23 -0400

    shadows

--- MiniGame/screens/StartGameScreen.js
+++ MiniGame/screens/StartGameScreen.js
@@ -13,9 +13,19 @@ function StartGameScreen() {
 
 const styles = StyleSheet.create({
     inputContainer: { 
-      flex: 1,
       marginTop: 100,
+      marginHorizontal: 24,
       padding: 16,
+      backgroundColor: "#72063c",
+      borderRadius: 8,
+      elevation: 4,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 4
+      },
+      shadowRadius: 6,
+      shadowOpacity: 0.26,
     }
 });
 
