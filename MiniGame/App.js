7fc4c5408a8f4f57cc7814db68f8db6861ab961e
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-18 17:31:47 -0400

    gradient

--- MiniGame/App.js
+++ MiniGame/App.js
@@ -1,8 +1,17 @@
 import { StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={['#70043a', '#ddb52f']} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+});
