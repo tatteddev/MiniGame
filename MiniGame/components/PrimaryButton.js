3695fa080bcb8b2211fe05870ff28c4b5a3be0a4
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-18 16:58:00 -0400

    buttons

--- MiniGame/components/PrimaryButton.js
+++ MiniGame/components/PrimaryButton.js
@@ -1,11 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log('pressed');
+  }
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable onPress={pressHandler}>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: "#72063c",
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    margin: 4,
+    elevation: 4,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    textAlign: "center",
+  }
+});
 
 export default PrimaryButton;
--- MiniGame/screens/StartGameScreen.js
+++ MiniGame/screens/StartGameScreen.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4d0428",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "#000",
