5f88eeb7c46ac8a2d477e946d05a27642d140d6e
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-22 17:10:20 -0400

    safe area

--- MiniGame/App.js
+++ MiniGame/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ImageBackground } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
@@ -25,7 +25,9 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>
+          {screen}
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- MiniGame/screens/GameScreen.js
+++ MiniGame/screens/GameScreen.js
@@ -1,7 +1,25 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function GameScreen() {
-    return <Text>The Game Screen!</Text>;
+    return (
+    <View style={styles.screen}>
+        <Text>Opponent's Guess</Text>
+        {/* Guess */}
+        <View>
+            <Text>LOWER</Text>
+            <Text>HIGHER</Text>
+            {/* +- */}
+        </View>
+        {/* <View>Log Rounds</View> */}
+    </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24,
+    }
+});
 
 export default GameScreen;
